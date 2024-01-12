a287c3921907eabb4c245300e0c9226eaf7d66e1
Pranesh <me.praneshkumar@gmail.com>
2024-01-13 00:46:21 +0530

    switched to ms sql server as db

--- researchproject/settings.py
+++ researchproject/settings.py
@@ -76,33 +76,33 @@ WSGI_APPLICATION = "researchproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'sql_server.pyodbc',
-#         'NAME': 'researchdata',
-#         'USER': 'dbadmin',
-#         'PASSWORD': 'Db4SSNIT',
-#         'HOST': 'ssn-it-researchdata.database.windows.net',
-#         'PORT': '1433',
-#         'OPTIONS': {
-#             'driver': 'ODBC Driver 18 for SQL Server',
-#             'encrypt': 'yes',
-#             'trust_server_certificate': 'no',
-#             'connection_timeout': 30,
-#         },
-#     },
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "ssn_new",
-        "USER": "root",
-        "HOST": "localhost",
-        "PORT": 3306,
-        "PASSWORD": "Test-Database4Me",
-    }
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'researchdata',
+        'USER': 'dbadmin',
+        'PASSWORD': 'Db4SSNIT',
+        'HOST': 'ssn-it-researchdata.database.windows.net',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 18 for SQL Server',
+            'encrypt': 'yes',
+            'trust_server_certificate': 'no',
+            'connection_timeout': 30,
+        },
+    },
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "ssn_new",
+#         "USER": "root",
+#         "HOST": "localhost",
+#         "PORT": 3306,
+#         "PASSWORD": "Test-Database4Me",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
