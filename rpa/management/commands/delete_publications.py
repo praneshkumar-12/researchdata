676face61f0dbc401b56633337a76091be8bd055
lewin-jesudhas <136236663+lewin-jesudhas@users.noreply.github.com>
2024-05-23 14:33:05 +0530

    delete using uniqueid and latest excel

---
+++ rpa/management/commands/delete_publications.py
@@ -0,0 +1,40 @@
+import csv
+from django.core.management.base import BaseCommand
+from rpa.models import Publications
+
+
+class Command(BaseCommand):
+
+    help = 'Delete records from the Publications model from a CSV file based on uniqueIDs'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', type=str, help='Path to the CSV file')
+
+            
+    def handle(self, *args, **kwargs):
+        csv_file = kwargs['csv_file']
+        unique_ids = self.read_id(csv_file)
+        if unique_ids:
+            for unique_id in unique_ids:
+                temp = Publications.objects.filter(uniqueid=unique_id)
+                if temp:
+                    temp.delete()
+                    self.stdout.write(self.style.SUCCESS(f'Publication {unique_id} deleted successfully.'))
+                else:
+                    self.stdout.write(self.style.ERROR(f'Publication {unique_id} not deleted. Publication not found.'))
+
+
+    def read_id(self, csv_file):
+        with open(csv_file,'r', encoding='utf-8') as f:
+            unique_ids = []
+            reader = csv.reader(f)
+            for row in reader:
+                if row:
+                    unique_ids.append(row[0])
+        return unique_ids
+                    
+
+            
+
+
+
